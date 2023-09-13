9bf78fcc8d114c3d3aaf994f937f696c5aee4cb1
linxOD <linxOD@users.noreply.github.com>
2023-09-13 01:30:33 +0000

    deploy: 0fcb9ed572f4edacc45b5833cb04e58edfe5f766

--- js/mark.js
+++ js/mark.js
@@ -1,41 +1,38 @@
 // the DOM object of the context (where to search for matches)
-var markInstance = new Mark(document.querySelectorAll("p.yes-index"));
-// Cache DOM elements
-var keywordInput = document.querySelector("input[name='keyword']");
-var optionInputs = document.querySelectorAll("input[name='opt[]']");
+var input = document.querySelectorAll(".card-body.yes-index"),
+  markInstance = new Mark(input),
+  // Cache DOM elements
+  keywordInput = document.querySelector("input[name='keyword']"),
+  optionInputs = document.querySelectorAll("input[name='opt[]']"),
+  clearBtn = document.querySelector("button[data-search='clear']"),
+  prevBtn = document.querySelector("button[data-search='prev']"),
+  nextBtn = document.querySelector("button[data-search='next']"),
+  results = [],
+  currentClass = "current",
+  // offsetTop = 50,
+  currentIndex = 0;
 
-function performMark() {
-  // Read the keyword
-  var keyword = keywordInput.value;
-
-  // Determine selected options
-  var options = {
-    "ignoreJoiners": true,
-    "acrossElements": false,
-    "wildcards": "enabled",
-    "ignorePunctuation": [",", ".", ":", ";", "!", "?", "(", ")", "[", "]", "{", "}", "'"]
-  };
-  [].forEach.call(optionInputs, function (opt) {
-    options[opt.value] = opt.checked;
-  });
-
-  // Remove previous marked elements and mark
-  // the new keyword inside the context
-  markInstance.unmark({
-    done: function () {
-      markInstance.mark(keyword, options);
-    },
-  });
+function jumpTo() {
+  if (results.length) {
+    var position,
+      current = results[currentIndex];
+    [].forEach.call(results, function (el) {
+      el.classList.remove(currentClass);
+    });
+    if (current) {
+      current.classList.add(currentClass);
+      current.scrollIntoView({
+        block: "start",
+        inline: "nearest",
+        smooth: "true"
+      });
+    }
+  }
 }
 
 function performMarkUrl() {
   // Determine selected options
-  var options = {
-    "ignoreJoiners": true,
-    "acrossElements": false,
-    "wildcards": "enabled",
-    "ignorePunctuation": [",", ".", ":", ";", "!", "?", "(", ")", "[", "]", "{", "}", "'"]
-  };
+  var options = {};
   [].forEach.call(optionInputs, function (opt) {
     options[opt.value] = opt.checked;
   });
@@ -56,6 +53,67 @@ function performMarkUrl() {
     });
   }
 }
+
+function performMark() {
+  // Read the keyword
+  var keyword = keywordInput.value;
+
+  // Determine selected options
+  var options = {
+    done: function() {
+      results = input[0].querySelectorAll("mark");
+      currentIndex = 0;
+      jumpTo();
+    }
+  };
+  [].forEach.call(optionInputs, function (opt) {
+    options[opt.value] = opt.checked;
+  });
+
+  // Remove previous marked elements and mark
+  // the new keyword inside the context
+  markInstance.unmark({
+    done: function () {
+      markInstance.mark(keyword, options);
+    },
+  });
+}
+
+clearBtn.addEventListener("click", function() {
+  markInstance.unmark();
+  keywordInput.value = "";
+  keywordInput.scrollIntoView({
+    block: "start",
+    inline: "nearest",
+    smooth: "true"
+  });
+});
+
+nextBtn.addEventListener("click", function() {
+  if (results.length) {
+    currentIndex += this === nextBtn ? 1 : -1;
+    if (currentIndex < 0) {
+      currentIndex = results.length - 1;
+    }
+    if (currentIndex > results.length - 1) {
+      currentIndex = 0;
+    }
+    jumpTo();
+  }
+});
+
+prevBtn.addEventListener("click", function() {
+  if (results.length) {
+    currentIndex += this === prevBtn ? -1 : 1;
+    if (currentIndex < 0) {
+      currentIndex = results.length - 1;
+    }
+    if (currentIndex > results.length - 1) {
+      currentIndex = 0;
+    }
+    jumpTo();
+  }
+});
 
 // Listen to input and option changes
 keywordInput.addEventListener("input", performMark);
