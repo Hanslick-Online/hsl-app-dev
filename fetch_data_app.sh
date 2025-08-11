4255dbe1fc97695ea56723d0bbd575cf63040f2e
Fernando Sanz-Lázaro <54422069+fsanzl@users.noreply.github.com>
2025-08-11 13:01:42 +0200

    Update fetch_data_app.sh

--- fetch_data_app.sh
+++ fetch_data_app.sh
@@ -4,6 +4,7 @@ rm -rf hsl-app-dev* build_app html xslt
 curl -L -o dev.zip https://github.com/Hanslick-Online/hsl-app/archive/refs/heads/44-full-text-search-more-fine-grained-to-enable-context-search.zip
 #unzip dev
 unzip dev.zip
+mv  hsl-app-44-full-text-search-more-fine-grained-to-enable-context-search/ hsl-app-dev
 mv hsl-app-dev/*.* ./
 mv hsl-app-dev/{xslt,html,build_app} .
 chmod +x build_app/python/*py build_app/shell/*
