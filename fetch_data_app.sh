f8ecb09c2994783d8923d376a042c3a952024617
Fernando Sanz-Lázaro <54422069+fsanzl@users.noreply.github.com>
2026-06-18 14:42:42 +0200

    Modify fetch_data_app.sh to use branch argument

--- fetch_data_app.sh
+++ fetch_data_app.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-rm -rf hsl-app-dev* build_app html xslt
+if [ $# -eq 0 ]; then
+  BRANCH=dev
+else
+  BRANCH="$1"
+else
+
+fi
+rm -rf hsl-app-* build_app html xslt
 #wget https://github.com/Hanslick-Online/hsl-app/archive/refs/heads/dev.zip
-curl -L -o dev.zip https://github.com/Hanslick-Online/hsl-app/archive/refs/heads/dev.zip
+curl -L -o ${BRANCH}.zip https://github.com/Hanslick-Online/hsl-app/archive/refs/heads/${BRANCH}.zip
 #curl -L -o dev.zip https://github.com/Hanslick-Online/hsl-app/archive/refs/heads/44-full-text-search-more-fine-grained-to-enable-context-search.zip
 #unzip dev
-unzip dev.zip
+unzip ${BRANCH}.zip
 mv  hsl-app-44-full-text-search-more-fine-grained-to-enable-context-search/ hsl-app-dev
-mv hsl-app-dev/*.* ./
-mv hsl-app-dev/{xslt,html,build_app} .
+mv hsl-app-${BRANCH}/*.* ./
+mv hsl-app-${BRANCH}/{xslt,html,build_app} .
 chmod +x build_app/python/*py build_app/shell/*
-rm -rf hsl-app-dev*
+rm -rf hsl-app-${BRANCH}*
