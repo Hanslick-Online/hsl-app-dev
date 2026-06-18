db691d6f168b80111dceee32e71a9ea14402de0f
Fernando Sanz-Lázaro <54422069+fsanzl@users.noreply.github.com>
2026-06-18 14:53:24 +0200

    Update fetch_data_app.sh to use dynamic branch name

--- fetch_data_app.sh
+++ fetch_data_app.sh
@@ -4,13 +4,14 @@ if [ $# -eq 0 ]; then
 else
   BRANCH="$1"
 fi
+echo ${BRANCH}
 rm -rf hsl-app-* build_app html xslt
 #wget https://github.com/Hanslick-Online/hsl-app/archive/refs/heads/dev.zip
 curl -L -o ${BRANCH}.zip https://github.com/Hanslick-Online/hsl-app/archive/refs/heads/${BRANCH}.zip
 #curl -L -o dev.zip https://github.com/Hanslick-Online/hsl-app/archive/refs/heads/44-full-text-search-more-fine-grained-to-enable-context-search.zip
 #unzip dev
 unzip ${BRANCH}.zip
-mv  hsl-app-44-full-text-search-more-fine-grained-to-enable-context-search/ hsl-app-dev
+#mv  hsl-app-44-full-text-search-more-fine-grained-to-enable-context-search/ hsl-app-${BRANCH}
 mv hsl-app-${BRANCH}/*.* ./
 mv hsl-app-${BRANCH}/{xslt,html,build_app} .
 chmod +x build_app/python/*py build_app/shell/*
